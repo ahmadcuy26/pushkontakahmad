099f16face3528b6b41e7d7abdae793d74791c76
ahmadcuy26 <rizkiahmadf761@gmail.com>
2023-06-25 17:19:11 +0700

    bebas

---
+++ edit_info.js
@@ -0,0 +1,27 @@
+const { modul } = require('./module');
+const edit_info = `Di Bawah`// ga usah di otak atik
+
+const urlpepe = `https://telegra.ph/file/4a898640fa32d4018a384.jpg`// url/link foto
+
+const namaowner = `4K Sanzz`// nama owner
+const noowner = `6281236167286`// nomor owner
+
+const packname = `Fxyz-Botz`// nama pack sticker
+const author = `4K Sanzz`// nama author stiker
+
+const pending = `2000`// waktu tunggu broadcast
+/**EDIT WAKTU TUNGGU BROADCAST DI ATAS
+CONTOH :
+1000 = 1 DETIK
+2000 = 2 DETIK
+3000 = 3 DETIK
+DAN SETERUS NYA
+
+🚫TIDAK DI SARAN KAN UNTUK DI BAWAH 1 DETIK🚫
+KARENA AKAN MENYEBAB KAN WA TERBLOKIR
+
+BELI SCRIPT NO ENC, CHAT WHATSAPP DI BAWAH
+HARGA : Rp 50.000
+NOMOR WA : 6281236167286
+**/
+module.exports = { edit_info,urlpepe,namaowner,noowner,packname,author,pending }
